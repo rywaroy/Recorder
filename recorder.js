e1d6495699de2cfd3c1e6578953ce8cdb2a7ace6
roy <464485682@qq.com>
2022-07-25 15:16:12 +0800

    feat: support

--- recorder.js
+++ recorder.js
@@ -1,5 +1,7 @@
 class Recorder {
     static Stream;
+    static Ctx;
+    static Scope;
     static BufferSize = 4096;
 
     static IsOpen () {
@@ -13,5 +15,38 @@ class Recorder {
             };
         };
         return false;
+    }
+
+    static Support() {
+        const AC = window.AudioContext;
+        if(!AC){
+            AC = window.webkitAudioContext;
+        };
+        if(!AC){
+            return false;
+        };
+        const scope = navigator.mediaDevices || {};
+        if(!scope.getUserMedia){
+            scope = navigator;
+            scope.getUserMedia || (scope.getUserMedia = scope.webkitGetUserMedia || scope.mozGetUserMedia || scope.msGetUserMedia);
+        };
+        if(!scope.getUserMedia){
+            return false;
+        };
+        
+        Recorder.Scope = scope;
+        if(!Recorder.Ctx || Recorder.Ctx.state === 'closed'){
+            //不能反复构造，低版本number of hardware contexts reached maximum (6)
+            Recorder.Ctx = new AC();
+            
+            Recorder.BindDestroy('Ctx', function() {
+                const ctx = Recorder.Ctx;
+                if (ctx && ctx.close) {//能关掉就关掉，关不掉就保留着
+                    ctx.close();
+                    Recorder.Ctx = 0;
+                };
+            });
+        };
+        return true;
     }
 }
